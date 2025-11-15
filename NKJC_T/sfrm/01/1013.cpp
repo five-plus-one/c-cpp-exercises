9bb649b9be5765b085867c1cfa3da0ede2a7d103
five-plus-one <5plus1@five-plus-one.com>
2025-11-15 09:54:55 +0800

    1

--- NKJC_T/sfrm/01/1013.cpp
+++ NKJC_T/sfrm/01/1013.cpp
@@ -6,18 +6,16 @@ const int N= 2*1e5+10;
 struct st{
     int dis;
     int num;
+    ll qzh;
 }p[N];
 int n,minpos,currentpos;
 ll currentdistance,mindistance;
-bool comp(st a,st b){
-    return a.dis<b.dis;
-}
+
 int main(){
     scanf("%d",&n);
     for(int i=1;i<=n;i++){
         scanf("%d",&p[i].dis);
     }
-    currentpos = 1;
     minpos = 1;
     currentdistance = 0;
     for(int i=1;i<=n;i++){
@@ -25,12 +23,14 @@ int main(){
     }
     // sort(p+1,p+1+n,comp);
     int startdis = p[1].dis;
+    p[0].qzh = 0; p[1].qzh =p[1].num;
     for(int i=2;i<=n;i++){
         currentdistance+=(p[i].dis-startdis)*p[i].num;
+        p[i].qzh = p[i-1].qzh + p[i].num;
     }
     mindistance = currentdistance;
     for(int i=2;i<=n;i++){
-        currentdistance+=(p[i-1].num-p[i].num)*(p[i].dis-p[i-1].dis);
+        currentdistance+=(p[i-1].qzh - (p[n].qzh - p[i-1].qzh))*(p[i].dis-p[i-1].dis);
         if(currentdistance < mindistance){
             mindistance = currentdistance;
             minpos = i;
