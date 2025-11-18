d8ec7123ef25fa5137155ff1c3f4c2875d7c2a29
five-plus-one <5plus1@five-plus-one.com>
2025-11-18 23:43:17 +0800

    A

---
+++ NKJC_T/2/B.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin>>n;
+    cout<<"1 ";
+    for(int i=3;i<=n;i+=2){
+        cout<<i<<" "<<i-1<<" ";
+    }
+    if(n%2==0){
+        cout<<n;
+    }
+}
---
+++ NKJC_T/2/D.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+using ll = long long;
+int n,k,m;
+const int N =1e6+10;
+int map[N];
+int main(){
+    scanf("%d%d",&n,&k);
+    m=-1;
+    int num,lastnum = -1,numcnt = 0;
+    for(int i=1;i<=n;i++){
+        scanf("%1d",&num);
+        if(num!=lastnum){
+            map[++m] = numcnt;
+            numcnt = 1;
+            lastnum = num;
+        }else{
+            numcnt++;
+        }
+    }
+    map[++m] = numcnt;
+    // for(int i=1;i<=m;i++){
+    //     cout<<map[i]<<endl;
+    // }
+    ll ans = 0;
+    if(k==1){
+        for(int i=1;i<=m;i++){
+            ans+=((map[i]+1)*map[i]/2);
+        }
+    }else{
+        for(int i=1;i<=m-k+1;i++){
+            ans+=(map[i]*map[i+k-1]);
+        }
+    }
+    printf("%lld",ans);
+    return 0;
+}
