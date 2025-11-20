fe498d8a63c9c0af7d800a35ae7ab6751f697f9d
five-plus-one <5plus1@five-plus-one.com>
2025-11-20 11:25:02 +0800

    1023_1

---
+++ NKJC_T/sfrm/01/1023.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<cstring>
+using namespace std;
+const int N = 1e6+10;
+int nextc[40][N],lastc[40],n,q,m;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    memset(lastc,-1,sizeof(lastc));
+    string s,t;
+    cin>>s;
+    n = s.length();
+    for(int i=n-1;i>=0;i--){
+        for(int j=0;j<26;j++){
+            nextc[j][i] = lastc[j];
+        }
+        lastc[s[i]-'a'] = i;
+    }
+    for(int i=0;i<26;i++){
+        cout<<lastc[i]<<endl;
+    }
+
+
+    cin>>q;
+    while(q--){
+        cin>>t;
+        m = t.length();
+        int pos = 0;
+        for(int i=0;i<m;i++){
+            pos = nextc[pos][t[i]];
+            cout<<pos<<endl;
+            if(pos == -1) break;
+        }
+        if(pos == -1){
+            cout<<"NO\n";
+        }else{
+            cout<<"YES\n";
+        }
+    }
+}
