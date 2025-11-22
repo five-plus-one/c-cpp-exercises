58b6653dc517b1e63b3c1f364c41d65c5d2e5637
five-plus-one <5plus1@five-plus-one.com>
2025-11-22 13:53:57 +0800

    1024_1

---
+++ NKJC_T/sfrm/01/1024_1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    string a[30];
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
+    }
+    sort(a+1,a+n+1);
+    for(int i=n;i>=1;i--){
+        cout<<a[i];
+    }
+}
