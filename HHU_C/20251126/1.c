46715625ba6f438d30211467b1db545d0c684a7a
five-plus-one <5plus1@five-plus-one.com>
2025-11-27 10:57:04 +0800

    test

--- HHU_C/20251126/1.c
+++ HHU_C/20251126/1.c
@@ -2,10 +2,11 @@
 #include<string.h>
 int main(){
     char a[114] = {'1','1','4','5','1','4','\0','1','9','1','9'};
-    printf("%llu\n",sizeof("abc\0abc"));
-    printf("%llu\n",sizeof("abc\0"));
-    printf("%llu\n",sizeof("abc"));
-    printf("%llu\n",sizeof("abc\0\0"));
-    printf("%s %llu\n",a,sizeof(a));
-    printf("%llu %lld %lld\n",sizeof("a"),strlen("a"),strlen(a));
+    // printf("%llu\n",sizeof("abc\0abc"));
+    // printf("%llu\n",sizeof("abc\0"));
+    // printf("%llu\n",sizeof("abc"));
+    // printf("%llu\n",sizeof("abc\0\0"));
+    // printf("%s %llu\n",a,sizeof(a));
+    // printf("%llu %lld %lld\n",sizeof("a"),strlen("a"),strlen(a));
+    printf("%d %d %d\n",strcmp("abc","abc\0d"),strcmp("x","abc"),strcmp("114","114"));
 }
