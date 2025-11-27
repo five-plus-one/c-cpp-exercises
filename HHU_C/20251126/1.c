a3249c29b2d208e1e0d9017a45efd286766f722a
five-plus-one <5plus1@five-plus-one.com>
2025-11-27 10:33:50 +0800

    test

--- HHU_C/20251126/1.c
+++ HHU_C/20251126/1.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
     char a[114] = {'1','1','4','5','1','4','\0','1','9','1','9'};
     printf("%llu\n",sizeof("abc\0abc"));
@@ -6,5 +7,5 @@ int main(){
     printf("%llu\n",sizeof("abc"));
     printf("%llu\n",sizeof("abc\0\0"));
     printf("%s %llu\n",a,sizeof(a));
-    printf("%d\n",sizeof("a"));
+    printf("%llu %lld %lld\n",sizeof("a"),strlen("a"),strlen(a));
 }
