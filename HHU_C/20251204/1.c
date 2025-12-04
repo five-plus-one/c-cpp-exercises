bf9a4d1f2f7375e87a866db462fc9995e9f5de05
five-plus-one <5plus1@five-plus-one.com>
2025-12-04 10:10:11 +0800

    1

--- HHU_C/20251204/1.c
+++ HHU_C/20251204/1.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
     int a = 10;double b = 20.114;
     double *p2 = &b;
     int *p = &a;
     printf("%p %d %p %f\n",p,*p,p2,*p2);
-    printf("%llu %llu",sizeof(p),sizeof(p2));
+    printf("%llu %llu\n",sizeof(p),sizeof(p2));
+    char a3[100];
+    char *p3 = a3;
+    scanf("%s",a3);
+    printf("%llu %llu %llu %llu",sizeof(a3),sizeof(char),sizeof(p3),strlen(a3));
 }
