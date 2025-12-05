7f650c1aa04277a875005c8d3eb4d173813a6719
five-plus-one <5plus1@five-plus-one.com>
2025-12-05 16:12:17 +0800

    D

---
+++ HHU/2025xs_d/D.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+long long T,n,x,y,z; 
+long long solve(int n,int x,int y,int z){
+	long long ans = 0;
+	for(long long i=1;i<=n;i++){
+		for(long long j=0;j<=n-i;j++){
+			long long m =n-i-j;
+			long long H = m*j*x + (m*y) + j*(j-1)/2*x;
+			long long cur = H *i + (1+i)*i/2 * j*x + i*z;
+//			cout<<i<<" "<<j<<" "<<m<<" "<<cur<<endl;
+			ans = max(ans,cur);
+		}
+	}
+	return ans;
+}
+int main(){
+    scanf("%d",&T);
+    while(T--){
+    	scanf("%d %d %d %d",&n,&x,&y,&z);
+    	long long ans = solve(n,x,y,z);
+    	printf("%lld\n",ans);
+	}
+}
